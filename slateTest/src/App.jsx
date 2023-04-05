d7ba1c07cc15503d5b50d5295ae324a7115f0bf4
jkaagh <69788622+jkaagh@users.noreply.github.com>
2023-04-05 10:31:15 +0200

    update template
    installed slate and react dom, put template code.

--- slateTest/src/App.jsx
+++ slateTest/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import React, { useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 
+const SimpleSlateEditor = () => {
+  // Define the initial state of the editor.
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A simple Slate.js editor." }],
+    },
+  ]);
 
+  // Create the Slate.js editor instance.
+  const editor = useMemo(() => withReact(createEditor()), []);
 
-function App() {
-
-    
   return (
-    <div className="App">
-      
-    </div>
-  )
-}
+    <Slate editor={editor} value={value} onChange={(newValue) => setValue(newValue)}>
+      <Editable />
+    </Slate>
+  );
+};
 
-export default App
+export default SimpleSlateEditor;
