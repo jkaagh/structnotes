7af5148f353bc6464958ff76933d08261db1b19b
jkaagh <69788622+jkaagh@users.noreply.github.com>
2023-04-05 20:39:53 +0200

    worked on accordion

--- slateTest/src/components/editorComponents/AccordionElement.jsx
+++ slateTest/src/components/editorComponents/AccordionElement.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AccordionElement = ({ attributes, children }) => {
-  return (
-    <div {...attributes} className="ml-3 pl-2 border-l border-slate-700">
-        
-        <div className="p-2 bg-slate-700">
 
+    const [closed, setClosed] = useState(false); //todo eventually fetch starting position from savefile
+    const [heightClass, setHeightClass] = useState('');
+  
+    const handleToggle = () => {
+      if (closed === false) {
+        setHeightClass('h-0');
+      } else {
+        setHeightClass('');
+      }
+  
+      setClosed(!closed);
+    };
+
+    
+    return (
+        <div {...attributes} className="ml-3 pl-2 border-l border-slate-700">
+
+          {children}
         </div>
-        
-        <div>
-            {children}
-        </div>
-    </div>
-  );
+    );
 };
+
+
 
 export default AccordionElement;
